6daed6de063d47427e05059628f079d972865378
Jared A. Greathouse <82923168+jgreathouse9@users.noreply.github.com>
2025-01-03 10:59:47 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,8 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../mlsynth'))  # Adjust path based on where conf.py is
+
+
 # Configuration file for the Sphinx documentation builder.
 
 # -- Project information
