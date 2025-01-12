00239398b9c9fa2d29d4041b22c7b9fb613aa24a
Jared A. Greathouse <82923168+jgreathouse9@users.noreply.github.com>
2025-01-12 15:33:47 -0500

    Update exampleplot.py

--- docs/auto_examples/exampleplot.py
+++ docs/auto_examples/exampleplot.py
@@ -4,11 +4,34 @@ Example Plot
 This is a simple example of how to create a plot using matplotlib.
 """
 
-import matplotlib.pyplot as plt
+import pandas as pd
+from mlsynth.mlsynth import FMA
+import os
+from theme import jared_theme
+import matplotlib
 
-# Example plotting code
-plt.plot([1, 2, 3, 4], [1, 4, 9, 16])
-plt.xlabel('x label')
-plt.ylabel('y label')
-plt.title('Simple Plot')
-plt.show()
+# Load the CSV file using pandas
+df = pd.read_csv('https://raw.githubusercontent.com/jgreathouse9/mlsynth/refs/heads/main/basedata/HongKong.csv)
+
+treat = "Integration"
+outcome = "GDP"
+unitid = "Country"
+time = "Time"
+
+config = {
+    "df": df,
+    "treat": treat,
+    "time": time,
+    "outcome": outcome,
+    "unitid": unitid,
+    "counterfactual_color": "blue",
+    "treated_color": "black",
+    "display_graphs": True,
+    "save": save,
+    "criti": 10,  # Assuming Stationary
+    "DEMEAN": 1,  # Demeans the donor pool
+}
+
+model = FMA(config)
+
+FMAest = model.fit()
