c9f0bb05bb4af7bc65f185a8fc87023485472595
Jared A. Greathouse <82923168+jgreathouse9@users.noreply.github.com>
2025-01-12 16:24:06 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup, find_packages
 
+# Open README.md with UTF-8 encoding to avoid decoding issues
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
     name="mlsynth",
     version="0.1.0",
     description="A Python package for advanced synthetic control methods.",
-    long_description=open("README.md").read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     author="Jared Greathouse",
     author_email="jgreathouse3@student.gsu.edu",
