37ae4a94805f19821c2da2a9425d9ccae6b83318
Jared A. Greathouse <82923168+jgreathouse9@users.noreply.github.com>
2025-01-12 16:58:52 -0500

    Update exampleplot.py

--- docs/auto_examples/exampleplot.py
+++ docs/auto_examples/exampleplot.py
@@ -1,14 +1,31 @@
 """
-Example Plot
-=============
-This is a simple example of how to create a plot using matplotlib.
+FDID Estimation
+=================
+FDID Plot
 """
 
 import pandas as pd
 from mlsynth.mlsynth import FDID
 import os
 import matplotlib
-from theme import jared_theme
+
+jared_theme = {
+    "axes.grid": True,
+    "grid.linestyle": "-",
+    "grid.color": "black",
+    "legend.framealpha": 1,
+    "legend.facecolor": "white",
+    "legend.shadow": True,
+    "legend.fontsize": 14,
+    "legend.title_fontsize": 16,
+    "xtick.labelsize": 14,
+    "ytick.labelsize": 14,
+    "axes.labelsize": 16,
+    "axes.titlesize": 20,
+    "figure.dpi": 100,
+    "axes.facecolor": "#c7d0c9",
+    "figure.figsize": (10, 5.5),
+}
 
 matplotlib.rcParams.update(jared_theme)
 
