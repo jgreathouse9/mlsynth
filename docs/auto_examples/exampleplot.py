7615b52ae5a654b01cd46aaa0b0cc6f4d61639e3
Jared A. Greathouse <82923168+jgreathouse9@users.noreply.github.com>
2025-01-12 16:52:55 -0500

    Update exampleplot.py

--- docs/auto_examples/exampleplot.py
+++ docs/auto_examples/exampleplot.py
@@ -7,6 +7,9 @@ This is a simple example of how to create a plot using matplotlib.
 import pandas as pd
 from mlsynth.mlsynth import FDID
 import os
+from theme import jared_theme
+
+matplotlib.rcParams.update(jared_theme)
 
 # Load the CSV file using pandas
 df = pd.read_csv('https://raw.githubusercontent.com/jgreathouse9/mlsynth/refs/heads/main/basedata/HongKong.csv')
