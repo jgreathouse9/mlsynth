e572a3014248f49f14338c76638664a965d2857b
Jared A. Greathouse <82923168+jgreathouse9@users.noreply.github.com>
2025-01-12 13:46:15 -0500

    Create example_plot.py

---
+++ examples/example_plot.py
@@ -0,0 +1,15 @@
+"""
+Example Plot
+============
+
+This example demonstrates a simple plot using matplotlib.
+"""
+
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4]
+y = [10, 20, 25, 30]
+
+plt.plot(x, y)
+plt.title("Example Plot")
+plt.show()
