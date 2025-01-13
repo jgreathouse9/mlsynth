e357421ec774bf2648270b03fcc146538642ebfe
Jared A. Greathouse <82923168+jgreathouse9@users.noreply.github.com>
2025-01-12 19:32:37 -0500

    Update Prop99.py

--- docs/auto_examples/Prop99.py
+++ docs/auto_examples/Prop99.py
@@ -4,17 +4,33 @@ L2 Relaxer
 L2PDA
 """
 
-
 from mlsynth.mlsynth import PDA
 import matplotlib.pyplot as plt
 import pandas as pd
 import os
-import sys
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from theme import jared_theme
 import matplotlib
 
+jared_theme = {
+    "axes.grid": True,
+    "grid.linestyle": "-",
+    "grid.color": "black",
+    "legend.framealpha": 1,
+    "legend.facecolor": "white",
+    "legend.shadow": True,
+    "legend.fontsize": 14,
+    "legend.title_fontsize": 16,
+    "xtick.labelsize": 14,
+    "ytick.labelsize": 14,
+    "axes.labelsize": 16,
+    "axes.titlesize": 20,
+    "figure.dpi": 100,
+    "axes.facecolor": "#c7d0c9",
+    "figure.figsize": (10, 5.5),
+}
+
 matplotlib.rcParams.update(jared_theme)
+
 
 # Access the corresponding dictionary
 file_path = r'https://raw.githubusercontent.com/jgreathouse9/mlsynth/refs/heads/main/basedata/smoking_data.csv'
@@ -27,8 +43,6 @@ unitid = df.columns[0]
 time = df.columns[1]
 outcome = df.columns[2]
 treat = "Proposition 99"
-
-
 
 config = {
     "df": df,
