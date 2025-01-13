e8be389b23aa002475f80fdacd4badd661f7d6d3
Jared A. Greathouse <82923168+jgreathouse9@users.noreply.github.com>
2025-01-12 19:46:47 -0500

    Update resultutils.py

--- mlsynth/utils/resultutils.py
+++ mlsynth/utils/resultutils.py
@@ -79,7 +79,7 @@ def plot_estimates(
             label=label,
             color=color,
             linestyle="--",
-            linewidth=2,
+            linewidth=1.25,
             marker="o"
         )
 
