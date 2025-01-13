3b442a53853440cc970f47125d2b592d6035c1e6
Jared A. Greathouse <82923168+jgreathouse9@users.noreply.github.com>
2025-01-13 11:14:48 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ templates_path = ["_templates"]
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_material'
+html_theme = 'sphinx_book_theme'
 
 # -- Options for EPUB output
 epub_show_urls = "footnote"
