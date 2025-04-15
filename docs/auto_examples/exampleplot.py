c3b1b47ed3eecfa26b6f7b378865eee0659fc1e0
Jared A. Greathouse <82923168+jgreathouse9@users.noreply.github.com>
2025-04-15 19:34:30 -0400

    Update exampleplot.py

--- docs/auto_examples/exampleplot.py
+++ docs/auto_examples/exampleplot.py
@@ -7,27 +7,6 @@ FDID Plot
 import pandas as pd
 from mlsynth.mlsynth import FDID
 import os
-import matplotlib
-
-jared_theme = {
-    "axes.grid": True,
-    "grid.linestyle": "-",
-    "grid.color": "black",
-    "legend.framealpha": 1,
-    "legend.facecolor": "white",
-    "legend.shadow": True,
-    "legend.fontsize": 14,
-    "legend.title_fontsize": 16,
-    "xtick.labelsize": 14,
-    "ytick.labelsize": 14,
-    "axes.labelsize": 16,
-    "axes.titlesize": 20,
-    "figure.dpi": 100,
-    "axes.facecolor": "#c7d0c9",
-    "figure.figsize": (10, 5.5),
-}
-
-matplotlib.rcParams.update(jared_theme)
 
 # Load the CSV file using pandas
 df = pd.read_csv('https://raw.githubusercontent.com/jgreathouse9/mlsynth/refs/heads/main/basedata/HongKong.csv')
@@ -43,7 +22,7 @@ config = {
     "time": time,
     "outcome": outcome,
     "unitid": unitid,
-    "counterfactual_color": "blue",
+    "counterfactual_color": ["blue"],
     "treated_color": "black",
     "display_graphs": True,
     "criti": 10,  # Assuming Stationary
