9e78d7112ea760a6acb0470bfd102b756863b8dc
Jared A. Greathouse <82923168+jgreathouse9@users.noreply.github.com>
2025-05-30 17:56:39 -0400

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,28 @@
+from .estimators.tssc import TSSC ## Check
+from .estimators.fma import FMA ## Check
+from .estimators.pda import PDA ## Check
+from .estimators.fdid import FDID ## Check
+from .estimators.clustersc import CLUSTERSC ## Check
+from .estimators.proximal import PROXIMAL ## Check
+from .estimators.fscm import FSCM ## Check
+from .estimators.src import SRC ## Check
+from .estimators.scmo import SCMO
+from .estimators.si import SI ## Check
+from .estimators.nsc import NSC # Check
+from .estimators.sdid import SDID # Check
+
+# Define __all__ to specify the public API of the package
+__all__ = [
+    "TSSC",
+    "FMA",
+    "PDA",
+    "FDID",
+    "CLUSTERSC",
+    "PROXIMAL",
+    "FSCM",
+    "SRC",
+    "SCMO",
+    "SI",
+    "NSC",
+    "SDID",
+]
