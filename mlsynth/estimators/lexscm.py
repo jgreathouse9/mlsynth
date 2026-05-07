7a86c335b95dc3d4b274d56102537492321a569e
Jared A. Greathouse <82923168+jgreathouse9@users.noreply.github.com>
2026-05-07 15:48:35 -0400

    Update lexscm.py

--- mlsynth/estimators/lexscm.py
+++ mlsynth/estimators/lexscm.py
@@ -5,7 +5,7 @@ from typing import Any, Dict, Optional, List, Literal
 # Configuration and Exceptions
 from ..config_models import BaseMAREXConfig, LEXSCMConfig
 from ..exceptions import MlsynthDataError, MlsynthEstimationError
-from ..utils.helperutils import lexplot
+from ..utils.plotter import lexplot
 # Utilities - Data Handling
 from ..utils.datautils import balance
 
